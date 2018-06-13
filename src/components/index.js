5d4e9aac20199bb2a44385ef92cc3131513ed63e
georgi.antonov <gantonovfe@gmail.com>
2018-06-13 12:30:05 +0300

    export now works properly

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import VueStackedCarousel from '../components/VueStackedCarousel';
 import VueStackedCarouselExtended from '../components/VueStackedCarouselExtended';
 
-export default {
+export {
   VueStackedCarousel,
   VueStackedCarouselExtended,
 };
